47f119ad3da56b1fb115473ed6d47d1bec145e01
minzzang <minjjang1117@gmail.com>
2020-12-03 11:28:28 +0900

    어플리케이션

---
+++ src/main/java/com/mj/admin/Application.java
@@ -0,0 +1,12 @@
+package com.mj.admin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
