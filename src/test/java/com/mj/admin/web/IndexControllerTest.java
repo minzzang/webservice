3411b8b484e881758c64335736178b3582fd4687
minzzang <minjjang1117@gmail.com>
2020-12-12 16:05:16 +0900

    test 수정

--- src/test/java/com/mj/admin/web/IndexControllerTest.java
+++ src/test/java/com/mj/admin/web/IndexControllerTest.java
@@ -23,7 +23,7 @@ public class IndexControllerTest {
         String body = this.testRestTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링부트로 시작하는 웹 서비스 Ver.2");
+        assertThat(body).contains("안녕하세요");
     }
 
 }
